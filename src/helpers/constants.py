9f80644bd6ae63bcbcb5715933062b70db3a394d
soffos <csoffos@gmail.com>
2018-12-03 03:12:03 +0000

    changed seperator

--- src/helpers/constants.py
+++ src/helpers/constants.py
@@ -1,4 +1,4 @@
-TRIAGE_WEEKLY_MSG_TEMPLATE = "<!here> If you have the cycles, please sign up for a triage block or two for next week. The goal is double coverage for every triage block.\nCurrent schedule:```M - {} / {} / {}\nT - {} / {} / {}\nW - {} / {} / {}\nH - {} / {} / {}\nF - {} / {} / {}```"
+TRIAGE_WEEKLY_MSG_TEMPLATE = "<!here> If you have the cycles, please sign up for a triage block or two for next week. The goal is double coverage for every triage block.\nCurrent schedule:```M - {} | {} | {}\nT - {} | {} | {}\nW - {} | {} | {}\nH - {} | {} | {}\nF - {} | {} | {}```"
 TRIAGE_WEEKLY_MSG_ATTACHMENTS = [
   {
     "fallback": "Choose a day or three",
