283fe8667aed19f9912a23f7b11b808780765e28
Chris Soffos <csoffos@gmail.com>
2022-05-02 23:35:42 -0500

    updated for legibility

--- src/views.py
+++ src/views.py
@@ -6,6 +6,60 @@ from flask import request, Response, jsonify
 import sys
 import json
 
+def handle_day_selection(slack, data):
+  messageTs = data.get('message_ts', 0)
+  slack.post_ephemeral_message(
+    "Pick a time slot for {}:".format(data['actions'][0]['name'].capitalize()),
+    data.get('user', {}).get('id', ""),
+    attachments=messages.get_ephemeral_timeslot_attachments(data['actions'][0]['value'],messageTs)
+  )
+  # Should return to wipe the message
+  return ('',200)
+
+def handle_make_reservation(slack, data):
+  actionContent = data['actions'][0]
+  targetTs = actionContent.get('value', 0)
+
+  data_access.save_triage_reservation(data, targetTs)
+  messages.update_ephemeral(slack, data)
+  messages.update_weekly_message(slack, targetTs)
+  data['targetTs'] = targetTs
+  # Send calendar invite
+  handle_send_calendar_invite(s, data, actionContent)
+  return ('',200)
+
+def handle_send_calendar_invite(slack, data, meta):
+  try:
+    userInfo = slack.get_user_info(data['user']['id'])
+    userEmail = userInfo['user']['profile']['email']
+    # Need to ensure ephemeral button 'value' is original message ts for granularity
+    day = meta['name'][:3]
+    slot = int(meta['name'][-1:])
+    if slot == 5:
+      calendar.remove_invitation_by_epoch_day_slot(userEmail, float(data['targetTs']), day, None)
+    else:
+      slot = slot - 1 if slot < 4 else None
+      calendar.send_invite_by_epoch_day_slot(userEmail, float(data['targetTs']), day, slot)
+  except Exception as e:
+    # Don't fail just due to calendar failure
+    print("Failed to send calendar invite: {}".format(repr(e)))
+  return False
+
+def handle_get_leaderboard(slack, data):
+  messageTs = data['message']['ts']
+  if data.get('actions', [{}])[0].get('value', "") == "rep":
+    slack.post_ephemeral_message(
+      "Check out our leading triagers!\n{}\n>`Leaderboard captures 13 most recent triage rosters`".format(reporting.run_participation_report()),
+      data.get('user', {}).get('id', "")
+    )
+  else:
+    slack.post_ephemeral_message(
+      "Pick a time slot for {}:".format(data['user']['name'].capitalize()),
+      data.get('user', {}).get('id', ""),
+      attachments=messages.get_ephemeral_timeslot_attachments(data['actions'][0]['value'],messageTs)
+    )
+  return ('',200)
+
 @app.route("/triagemgmt/<meth>", methods=["POST"])
 @validate_slack_message
 def main(meth=None):
@@ -22,52 +76,15 @@ def main(meth=None):
   if requestDict:
     # Handle triage day selection
     if requestDict.get('callback_id', None) == "signup_dow_selection":
-      messageTs = requestDict.get('message_ts', 0)
-      s.post_ephemeral_message(
-        "Pick a time slot for {}:".format(requestDict['actions'][0]['name'].capitalize()),
-        requestDict.get('user', {}).get('id', ""),
-        attachments=messages.get_ephemeral_timeslot_attachments(requestDict['actions'][0]['value'],messageTs)
-      )
-      return ('',200)
-      # Should return something to wipe the message
-      # return "None or something"
+      return handle_day_selection
+
     elif requestDict.get('callback_id', None) == "signup_ts_selection":
       # Add reservation(s) to db and update slack message
-      actionContent = requestDict['actions'][0]
-      targetTs = actionContent.get('value', 0)
-      data_access.save_triage_reservation(requestDict, targetTs)
-      messages.update_ephemeral(s, requestDict)
-      messages.update_weekly_message(s, targetTs)
-      # Send calendar invite
-      try:
-        userInfo = s.get_user_info(requestDict['user']['id'])
-        userEmail = userInfo['user']['profile']['email']
-        # Need to ensure ephemeral button 'value' is original message ts for granularity
-        day = actionContent['name'][:3]
-        slot = int(actionContent['name'][-1:])
-        if slot == 5:
-          calendar.remove_invitation_by_epoch_day_slot(userEmail, float(targetTs), day, None)
-        else:
-          slot = slot - 1 if slot < 4 else None
-          calendar.send_invite_by_epoch_day_slot(userEmail, float(targetTs), day, slot)
-      except Exception as e:
-        print("Failed to send calendar invite: {}".format(repr(e)))
-      return ('',200)
+      return handle_make_reservation(s, requestDict)
+      
     elif requestDict.get('type', "") == "block_actions":
-      #import pprint;pprint.PrettyPrinter().pprint(requestDict);
-      messageTs = requestDict['message']['ts']
-      if requestDict.get('actions', [{}])[0].get('value', "") == "rep":
-        s.post_ephemeral_message(
-          "Check out our leading triagers!\n{}\n>`Leaderboard captures 13 most recent triage rosters`".format(reporting.run_participation_report()),
-          requestDict.get('user', {}).get('id', "")
-        )
-      else:
-        s.post_ephemeral_message(
-          "Pick a time slot for {}:".format(requestDict['user']['name'].capitalize()),
-          requestDict.get('user', {}).get('id', ""),
-          attachments=messages.get_ephemeral_timeslot_attachments(requestDict['actions'][0]['value'],messageTs)
-        )
-      return ('',200)
+      return handle_get_leaderboard(s, requestDict)
+
     print("requestDict:");print(requestDict)
   print("Request.data: {}".format(request.data))
   print("Request.form: {}".format(request.form))
@@ -80,6 +97,7 @@ def main(meth=None):
 def index():
   print("Received invalid request: /")
   return ('',500)
+
 @app.route('/<path:path>', methods=['GET', 'POST'])
 def catch_all(path):
   print("Received invalid request: {}".format(str(path)))
